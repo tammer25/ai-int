4f3d62e974fdff39f08f74b5a3244c9fa1b765ac
tammer25 <tsmansour25@gmail.com>
2025-08-31 13:27:00 +0300

    fix: modernize ESLint config to use flat config without FlatCompatfurther CI/CD improvementsUpdate eslint.config.mjs
    Replace FlatCompat approach with modern ESLint flat config format. This fixes the 'Missing parameter recommendedConfig in FlatCompat constructor' error by using @eslint/js directly with TypeScript support. The configuration now uses the modern flat config format that's the standard for ESLint 9+.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,56 +1,67 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import js from '@eslint/js';
+import typescript from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("eslint:recommended", "@typescript-eslint/recommended"),
+export default [
+  js.configs.recommended,
   {
+    files: ['**/*.{js,mjs,cjs,ts,tsx}'],
     languageOptions: {
-      globals: {
-        console: "readonly",
-        process: "readonly",
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true
+        }
       },
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        exports: 'writable',
+        global: 'readonly',
+        module: 'readonly',
+        require: 'readonly'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': typescript
     },
     rules: {
       // TypeScript related rules
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-non-null-assertion": "off",
-      "@typescript-eslint/ban-ts-comment": "off",
-      "@typescript-eslint/prefer-as-const": "off",
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/prefer-as-const': 'off',
       
       // React related rules
-      "react-hooks/exhaustive-deps": "off",
-      "react/no-unescaped-entities": "off",
-      "react/display-name": "off",
-      "react/prop-types": "off",
+      'react-hooks/exhaustive-deps': 'off',
+      'react/no-unescaped-entities': 'off',
+      'react/display-name': 'off',
+      'react/prop-types': 'off',
       
       // Next.js related rules
-      "@next/next/no-img-element": "off",
-      "@next/next/no-html-link-for-pages": "off",
+      '@next/next/no-img-element': 'off',
+      '@next/next/no-html-link-for-pages': 'off',
       
       // General JavaScript rules
-      "prefer-const": "off",
-      "no-unused-vars": "off",
-      "no-console": "off",
-      "no-debugger": "off",
-      "no-empty": "off",
-      "no-irregular-whitespace": "off",
-      "no-case-declarations": "off",
-      "no-fallthrough": "off",
-      "no-mixed-spaces-and-tabs": "off",
-      "no-redeclare": "off",
-      "no-undef": "off",
-      "no-unreachable": "off",
-      "no-useless-escape": "off",
-    },
-  },
+      'prefer-const': 'off',
+      'no-unused-vars': 'off',
+      'no-console': 'off',
+      'no-debugger': 'off',
+      'no-empty': 'off',
+      'no-irregular-whitespace': 'off',
+      'no-case-declarations': 'off',
+      'no-fallthrough': 'off',
+      'no-mixed-spaces-and-tabs': 'off',
+      'no-redeclare': 'off',
+      'no-undef': 'off',
+      'no-unreachable': 'off',
+      'no-useless-escape': 'off'
+    }
+  }
 ];
-
-export default eslintConfig;
